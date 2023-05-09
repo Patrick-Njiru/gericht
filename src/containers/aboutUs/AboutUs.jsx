53dd14c39532cf99a14a36418482599b9f4393f6
Patrick-Njiru <patricknjiru93@gmail.com>
2023-05-09 06:38:49 +0300

    add and style the about section.

--- src/containers/aboutUs/AboutUs.jsx
+++ src/containers/aboutUs/AboutUs.jsx
@@ -1,10 +1,40 @@
 import React from 'react'
 
+import images from '../../constants/images'
 import './aboutus.css'
 
 const AboutUs = () => {
   return (
-    <div>AboutUs</div>
+    <div className='about-us app__bg flex__center section__padding' id='about'>
+      <div className='about-us__overlay flex__center'>
+        <img src={images.G} alt='G overlay' />
+      </div>
+      
+      <div className='about-us__content flex__center'>
+        <div className='about-us__about'>
+          <h1 className='headtext__cormorant'> About Us </h1>
+          <img src={images.spoon} alt='about__spoon' className='spoon__image' />
+          <p className='p__opensans'> 
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae dicta consectetur incidunt nobis atque eum 
+          </p>
+          <button type='button' className='custom__button'> Know More </button>
+        </div>
+
+        <div className='about-us__knife flex__center'>
+          <img src={images.knife} alt='about knife' />
+        </div>
+
+        <div className='about-us__history'>
+          <h1 className='headtext__cormorant'> Our History </h1>
+          <img src={images.spoon} alt='about__spoon' className='spoon__image' />
+          <p className='p__opensans'> 
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae dicta consectetur incidunt nobis atque 
+          </p>
+          <button type='button' className='custom__button'> Know More </button>
+        </div>
+        
+      </div>
+    </div>
   )
 }
 
