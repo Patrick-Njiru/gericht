c5a6e7031a3aabcfba8ddabc46a74b8e9d177382
Patrick-Njiru <patricknjiru93@gmail.com>
2023-05-11 16:19:30 +0300

    add and style the laurels section.

--- src/containers/laurels/Laurels.jsx
+++ src/containers/laurels/Laurels.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
 
+import { SubHeading } from '../../components'
+import { images, data } from '../../constants'
 import './laurels.css'
+
+const AwardCard = ({ award: { imgUrl, title, subtitle } }) => (
+	<div className='awards-card'>
+		<img src={imgUrl} alt='award' />
+		<div className='awards__content'>
+			<p className='p__cormorant' style={{ color: '#dcca87' }}> { title } </p>
+			<p className='p__cormorant'> { subtitle } </p>
+		</div>
+	</div>
+)
 
 const Laurels = () => {
 	return (
-		<div>Laurels</div>
+		<div className='bg wrapper section-padding' id='awards'>
+			<div className='wrapper__info'>
+				<SubHeading title="Awards & Recognition" />
+				<h1 className='head-text__cormorant'> Our Laurels </h1>
+			</div>
+			<div className='laurels__awards'>
+				{data.awards.map(award => <AwardCard key={award.title} award={award} />)}
+			</div>
+			<div className='wrapper__img'>
+				<img src={images.laurels} alt='laurels' />
+			</div>
+		</div>
 	)
 }
 
